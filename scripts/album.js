a6aba58dfbd9022d367135bfc62ec8f68d690802
Dawna Ara <dawnaara@gmail.com>
2017-02-01 10:02:37 -0800

    assignment 25 completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumPicasso = {
      ]
  };
 
+ var albumBeck= {
+     title: 'Odeley',
+     artist: 'Beck',
+     label: 'EM',
+     year: '1998',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Lemons', duration: '1:01' },
+         { title: 'Odeley', duration: '5:01' },
+         { title: 'Jackass', duration: '3:21'},
+         { title: 'Fun', duration: '3:14' },
+         { title: 'Deborah', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,24 +55,19 @@ var albumPicasso = {
      return template;
  };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
  var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
-     // #3
      albumSongList.innerHTML = '';
- 
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -65,4 +75,14 @@ var albumPicasso = {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumBeck];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(album[index]);
+         index++;
+         if (index == album.length) {
+             index = 0;
+         }
+     });
  };
