3b626b86e1e099d26f8fec0b7fe15a9dd6983c7b
Dawna Ara <dawnaara@gmail.com>
2017-02-13 13:49:59 -0800

    fixed collection code

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,20 @@
- window.onload = function() {
+  var collectionItemTemplate =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+   ; 
+
+window.onload = function() {
      var collectionContainer = document.getElementsByClassName('album-covers')[0];
      collectionContainer.innerHTML = '';
  
@@ -6,3 +22,4 @@
          collectionContainer.innerHTML += collectionItemTemplate;
      }
  }
+ 
